899001bdacc51a0b984b607c48f56d744d99e234
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-03-21 23:50:25 +0600

    Add files via upload

---
+++ Myself/Get a Time.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "The time is " . date("h:i:sa") . "<br>"; 
+echo "The time is " . date("H:i:s");
+/* H - 24-hour format of an hour (00 to 23)
+h - 12-hour format of an hour with leading zeros (01 to 12)
+i - Minutes with leading zeros (00 to 59)
+s - Seconds with leading zeros (00 to 59)
+a - Lowercase Ante meridiem and Post meridiem (am or pm) */
+
+?>
+</body>
+</html>
